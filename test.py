307e6d0fd4898db3b8ecd51ac616c1d0fb59232c
kanegon0822 <73830895+kanegon0822@users.noreply.github.com>
2020-11-16 21:48:17 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+import os
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from linebot.models import messages
+
+# file=open('info.json','r')
+# info=json.load(file)
+
+
+
+# CHANNEL_ACCESS_TOKEN=info['CHANNEL_ACCESS_TOKEN']
+CHANNEL_ACCESS_TOKEN=os.environ['CHANNEL_ACCESS_TOKEN']
+line_bot_api=LineBotApi(CHANNEL_ACCESS_TOKEN)
+
+def main():
+    USER_ID=os.environ['USER_ID']
+    messages=TextSendMessage(text='こんにちは。これはテストです。')
+    line_bot_api.push_message(USER_ID,messages=messages)
+
+if __name__=='__main__':
+    main()
